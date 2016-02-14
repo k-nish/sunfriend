bf315acc7a183f2b97e0f63efa87acce2f56a1fc
Kouhei Nishimura <koh.oct.un@gmail.com>
2016-02-14 15:36:26 +0800

    ファイル整理とインデント整理

--- index.php
+++ index.php
@@ -114,11 +114,6 @@ if (isset($_POST['pass'])&&$_POST['pass']=='') {
     </div>
   </div>
 
-
-
-
-
-
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <!-- Include all compiled plugins (below), or include individual files as needed -->
